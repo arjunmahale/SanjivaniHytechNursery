13af6aa4e6ec4e20b8ef3cf2a946b0988d24cbcf
Arjun Mahale <maheshmahale588@gmail.com>
2026-04-17 09:55:56 +0530

    verticle scroll enabled on main page

--- js/main.js
+++ js/main.js
@@ -71,6 +71,9 @@
 	    animateIn: 'fadeIn',
 	    nav:false,
 	    autoplayHoverPause: false,
+	    touchDrag: false,
+	    mouseDrag: false,
+	    pullDrag: false,
 	    items: 1,
 	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
 	    responsive:{
